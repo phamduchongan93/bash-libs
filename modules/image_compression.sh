d36c4e597002a682c789ac69c77fc4bcce503413
An Pham <vincentpham1993@gmail.com>
2019-12-11 01:20:28 -0500

    update compression.sh
    - add new bzip2 compression

--- modules/image_compression.sh
+++ modules/image_compression.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-
-function tar_compress {
+# gunzip faster compressing speed, larger compression size
+function gz_compression {
   tar -cvzf "$1".tgz "$1"
 }
+
+# bzip2 slower compresss speed, but better compression
+function bzip2_compression {
+  tar -cjvf "$1".tar.bz2 "$1"
+}
+
+
 
 function zip_compress {
 }
